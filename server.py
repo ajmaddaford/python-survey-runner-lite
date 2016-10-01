c2b876886028a7e0c46b37e01ab1614d938b4851
Andrew Maddaford <ajmaddaford@gmail.com>
2016-10-01 15:16:09 +0100

    Add functionality to redirect to next page after saving

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, g
+from flask import Flask, request, redirect, jsonify, render_template, g
 from flask_sqlalchemy import SQLAlchemy
 import settings
 import logging
@@ -20,14 +20,28 @@ logging.basicConfig(level=settings.LOGGING_LEVEL, format=settings.LOGGING_FORMAT
 logger = wrap_logger(logging.getLogger(__name__))
 
 def load_schema_file(schema_filename):
-    schema_file = open(schema_filename, encoding="utf8")
-    return json.load(schema_file)
+    schema = g.get('schema', None)
+    if schema is None:
+        schema_file = open(schema_filename, encoding="utf8")
+        schema_json = json.load(schema_file)
+        g.schema = schema_json
+    return g.schema
 
 def get_block_json(survey_id, block_id):
     survey_json = load_schema_file(survey_id + '.json')
     for block in survey_json['groups'][0]['blocks']:
         if block['id'] == block_id:
             return block
+
+def get_next_block_id(block_id):
+    survey_json = g.schema
+    next_block = False
+    for block in survey_json['groups'][0]['blocks']:
+        if next_block:
+            return block['id']
+        if block['id'] == block_id:
+            next_block = True
+    return None
 
 def store_data(user_id, block_id, form_data):
     s = json.dumps(form_data)
@@ -60,7 +74,8 @@ def block(survey_id, block_id):
     f = form.generate_form(block_json, data)
     if f.validate_on_submit():
         store_data(user_id, block_id, f.data)
-        return render_template('thank-you.html')
+        next_block = get_next_block_id(block_id)
+        return redirect('/survey/' + survey_id + '/' + next_block)
     return render_template('survey.html', form=f)
 
 if __name__ == '__main__':
